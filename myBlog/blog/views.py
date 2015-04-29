c6ae9d4f071c5fe5da3011b23a431193138098a9
hkung77 <henrykung@live.ca>
2015-04-29 09:04:01 -0400

    TCK002_UI_Development : Setup urls.py and views.py to access the webpage

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import blogposts
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('blog/index.html')
+    context = RequestContext(request, {
+    })
+    return HttpResponse(template.render(context))
+    
