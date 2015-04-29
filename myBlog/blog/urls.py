708d485754dfa2f3391856e8ed0dbfc1acf88c71
hkung77 <henrykung@live.ca>
2015-04-29 11:00:38 -0400

    TCK002_UI_Development : Finished index.html as well as viewing each blog post

--- myBlog/blog/urls.py
+++ myBlog/blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+        url(r'(?P<ID>[0-9]+)/$', views.detail, name='detail'),
 ]
--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -7,8 +7,17 @@ from .models import blogposts
 # Create your views here.
 
 def index(request):
+    blog_list = blogposts.objects.order_by('-Pub_date')                 
     template = loader.get_template('blog/index.html')
     context = RequestContext(request, {
+        'blog_list' : blog_list 
     })
     return HttpResponse(template.render(context))
     
+def detail(request, ID="0"):
+    blog = blogposts.objects.get(id=ID)
+    template = loader.get_template('blog/post.html')
+    context = RequestContext(request, {
+        'blog':blog,
+    })
+    return HttpResponse(template.render(context))
