0fecfa0e5af9f56e3846eea7f6b6feb99507f0f3
hkung77 <henrykung@live.ca>
2015-04-30 00:13:34 -0400

    Adding more comments in code

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -11,11 +11,13 @@ from .models import blogposts
 logger = logging.getLogger(__name__)
 
 def index(request):
-# This function returns index.html ( the home page )
+# This function corresponds to index.html ( the home page )
+# It returns a list of all blogs in the database to index.html in a list view
     try:
         # Obtain all objects in the blog database
         blog_list = blogposts.objects.order_by('-Pub_date')     
     except blogposts.DoesNotExists: 
+        # Raise and error and log 
         raise CommandError('blogposts does not exists')
         logger.error('Attempting to access blogposts. Error: blogposts database does not exists')
 
@@ -30,11 +32,14 @@ def index(request):
     
 def detail(request, ID="0"):
 # This function is used for viewing individual blog posts
+# It takes 1 parameter ID (defaulted to 0) which corresponds to the primary key of each blog post in the database
+# Returns a single blog post with the corresponding ID with the detailed view of the blog
     try:
         # Obtain individual blog post via primary key
         blog = blogposts.objects.get(id=ID)                     
     except blogposts.id.DoesNotExists:
         # If primary key does not exists, blog is not in the database
+        # Raise and error and log 
         raise CommandError('blog post with '+ID+' does not exists in database')
         logger.error('Attempting to access blog post with '+ID+'. Error: ' +ID+' does not exists')
     # Set post.html as the template
